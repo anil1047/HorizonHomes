7798448ef2aa88766d209ab2273287d581cbba20
Anil Bijarniya <anilbijarniya1047@gmail.com>
2024-08-02 14:51:21 +0530

    Complete User update function working

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server message";
   return res.status(statusCode).json({
-    sucess: false,
+    success: false,
     statusCode,
     message,
   });
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import axios from "axios";
+
 import { useSelector } from "react-redux";
 import { useRef } from "react";
 import { app } from "../firebase";
@@ -10,13 +12,20 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 
+import { updateUserStart, updateUserFailure, updateUserSuccess } from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
+
 export default function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess]=useState(false);
+
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (file) {
@@ -47,6 +56,48 @@ export default function Profile() {
       }
     );
   };
+
+  const handleChange = (e)=>{
+    setFormData({ ...formData, [e.target.id] : e.target.value});
+  };
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    try{
+      dispatch(updateUserStart());
+      
+      const res = await fetch(`/api/user/update/${currentUser._id}`,{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+          // 'Authorization':'Bearer${access_token}',
+        },
+        body:JSON.stringify(formData),
+      });
+    
+    
+
+      const data=await res.json();
+
+      if(data.success ===false){
+        dispatch(updateUserFailure(data.message));
+        return ;
+      }
+
+      dispatch(updateUserSuccess(data)); 
+      setUpdateSuccess(true);
+
+
+    }
+    catch(error){
+      dispatch(updateUserFailure(error.message));
+    }
+  
+  };
+
+
+
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1
@@ -55,7 +106,7 @@ export default function Profile() {
       >
         Profile
       </h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -88,15 +139,19 @@ export default function Profile() {
         <input
           type="text"
           placeholder="username"
+          defaultValue={currentUser.username}
           id="username"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
-          placeholder="email"
+          placeholder="email"  
+          defaultValue={currentUser.email}
           id="email"
           className="
      border p-3 rounded-lg"
+     onChange={handleChange}
         />
         <input
           type="password"
@@ -106,13 +161,14 @@ export default function Profile() {
         />
 
         <button
+        disabled={loading}
           className="bg-slate-700 text-white
      rounded-lg p-3 uppercase hover:opacity-95 
      disabled:opacity-80"
         >
-          {" "}
-          Update{" "}
-        </button>
+          {loading ? 'Loading...':
+          'Update'}
+        </button>  
       </form>
       <div className="flex justify-between mt-5">
         <span
@@ -131,6 +187,8 @@ export default function Profile() {
           sign out
         </span>
       </div>
+      <p className="text-red-700 mt-5">{error ? error :''}</p>
+      <p className="text-green-700 mt-5">{updateSuccess ?'User updated successfully!':''}</p>
     </div>
   );
 }
