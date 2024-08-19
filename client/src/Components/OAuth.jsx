3243f8178672f12f224e5b8a1b096c057d21846d
Anil Bijarniya <anilbijarniya1047@gmail.com>
2024-08-19 09:24:59 +0530

    Update Error for google authentication

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -35,7 +35,7 @@ export default function OAuth() {
   return (
     <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg
     hover:opacity-95'>
-      Countinue with google
+      Continue with google
     </button>
   )
 }
